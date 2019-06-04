06f6bb9bb6e8797a4f01a8026140b952896a3720
natacha-h <natacha.heubes@hotmail.fr>
2019-06-04 14:04:52 +0200

    codage fonction rewarder qui renvoit l'ID du reward en fonction du nombre de points

---
+++ src/Utils/Rewarder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Utils;
+
+class Rewarder
+{
+
+    public function rewarder($points) // la fonction prend en paramètre le nombre de point de l'utilisateur
+    {
+        /*
+            - Le nombre de points détermine le reward
+            - on augmente de reward par palliers de 200 points
+            - il y a 6 palliers
+                
+         */
+        // si le nombre de points est > 1000, on est à reward = 5
+        if ($points >= 1000){
+            $reward = 6;
+        } 
+        elseif ($points <1000 && $points >= 800) {
+            $reward = 5;
+        }
+         elseif ($points <800 && $points >= 600) {
+            $reward = 4;
+        } 
+        elseif ($points < 600 && $points >= 600) {
+            $reward = 3;
+        } 
+        elseif ($points < 200 && $points >= 400) {
+            $reward = 2;
+        } else {
+            $reward = 1;
+        }
+
+        // on renvoit le reward à insérer
+        return $reward;
+
+    }
+
+
+}
