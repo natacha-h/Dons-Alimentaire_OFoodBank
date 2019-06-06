d76badf1a0667c2227d00c2686a099725c526252
Bastien2805 <bastien2805@hotmail.fr>
2019-06-06 09:04:28 +0200

    Ajout permission totale pour admin sur vision des profils

--- src/Security/ProfileVoter.php
+++ src/Security/ProfileVoter.php
@@ -2,8 +2,9 @@
 namespace App\Security;
 
 use App\Entity\User;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
 class ProfileVoter extends Voter
 {
@@ -22,14 +23,31 @@ class ProfileVoter extends Voter
         return true;
     }
 
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
     {
+        // Je récupère le user authentifié
         $authenticatedUser = $token->getUser();
 
         /** @var User $user */
         $user = $subject;
 
-        if($authenticatedUser === $user){ return true;}
+        // Si l'user authentifié correspond à l'id de user, retourne true, sinon renvoie un page 500 avec écrit "No Way"
+        if($authenticatedUser === $user)
+        {
+             return true;
+        }
+
+        if ($this->security->isGranted('ROLE_ADMIN')) {
+            return true;
+        }
+
         throw new \LogicException('No Way !');
     }
 }
